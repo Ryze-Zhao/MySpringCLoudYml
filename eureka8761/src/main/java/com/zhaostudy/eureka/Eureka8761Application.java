b3cea8157fe76e26ca0eb7913fe24730c2e15857
RyzeZhao <643080112@qq.com>
2018-10-19 10:46:32 +0800

    MySpringCloudYml：创建完毕eureka8761

---
+++ eureka8761/src/main/java/com/zhaostudy/eureka/Eureka8761Application.java
@@ -0,0 +1,12 @@
+package com.zhaostudy.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eureka8761Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka8761Application.class, args);
+    }
+}
